cad15f30e131680a5796badeba361c1d1862c1b1
protect-me <pmfwiww@gmail.com>
2021-07-06 16:09:05 +0900

    Feat: set dynamic meta info

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,13 @@ export default {
       { 'http-equiv' : 'X-UA-Compatible', content: 'IE=edge'},
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Nuxt Movie App' },
+      { hid: 'og:title', property: 'og:title', content: 'Nuxt Movie App / Search' },
+      { hid: 'og:description', property: 'og:description', content: 'The OMDb API is a RESTful web service to obtain movie information, all content and images on the site are contributed and maintained by our users.' },
+      { hid: 'og:image', property: 'og:image', content: 'https://heropy.blog/css/images/logo.png' },
+      { hid: 'og:url', property: 'og:url', content: process.env.CLIENT_URL },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
--- pages/movie/_id.vue
+++ pages/movie/_id.vue
@@ -112,6 +112,18 @@ export default {
         })
       return src
     }
+  },
+  head() {
+    return  {
+      meta : [
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:site_name', property: 'og:site_name', content: 'Nuxt Movie App' },
+        { hid: 'og:title', property: 'og:title', content: this.theMovie.Title },
+        { hid: 'og:description', property: 'og:description', content: this.theMovie.Plot },
+        { hid: 'og:image', property: 'og:image', content: this.theMovie.Poster },
+        { hid: 'og:url', property: 'og:url', content: `${process.env.CLIENT_URL}${this.$route.fullPath}` },
+      ]
+    }
   }
 }
 </script>
